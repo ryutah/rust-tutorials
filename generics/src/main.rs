b3ec4bb8236b09542142147eb43efffe3ce4c4d8
ryuta <ryuta.hara@topgate.co.jp>
2018-02-01 14:53:52 +0900

    Add: generics tutorial

---
+++ generics/src/main.rs
@@ -0,0 +1,36 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let float = Point { x: 1.0, y: 4.0 };
+    println!("p.x = {}", float.x());
+    println!("distance : {}", float.distance_from_origin());
+
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
