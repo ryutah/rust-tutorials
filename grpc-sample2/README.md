9765bba410e621ad3f85cbde9690fa4c2d4efadb
ryutah <ryuta0221@gmail.com>
2018-06-04 16:03:03 +0900

    Add: another grpc library example

---
+++ grpc-sample2/README.md
@@ -0,0 +1,3 @@
+# grpc-sample2
+
+grpc example use [stepancheg/grpc-rust](https://github.com/stepancheg/grpc-rust)
---
+++ grpc-sample2/src/lib.rs
@@ -0,0 +1,6 @@
+extern crate grpc;
+extern crate protobuf;
+extern crate tls_api;
+
+pub mod helloworld;
+pub mod helloworld_grpc;
